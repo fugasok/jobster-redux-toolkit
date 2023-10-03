53b2e605e7e98a8a04e5223ea52dbec1a07b3c0e
Tibor <tibor736@hotmail.com>
2023-10-03 19:21:37 +0300

    error page created

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom'
+import errorImg from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
+
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
+	return (
+		<Wrapper>
+			<div>
+				<img src={errorImg} alt='not found' />
+				<h3>Oh! Page not Found</h3>
+				<p>We can't seem to find the page you are looking for</p>
+				<Link to='/'>Back to homepage</Link>
+			</div>
+		</Wrapper>
+	)
 }
 export default Error
