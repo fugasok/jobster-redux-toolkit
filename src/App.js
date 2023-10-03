e0a4ac50ed07b93798fd425f0ff745ee0dcf3b2c
Tibor <tibor736@hotmail.com>
2023-10-03 14:36:54 +0300

    Added styled components library. Created html structure for landing page and styles addded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Landing from './pages/Landing'
+import styled from 'styled-components'
+
 const App = () => {
-  return (
-    <h1>Jobster</h1>
-  )
+	return (
+		<div>
+			<Landing />
+		</div>
+	)
 }
 export default App
+
