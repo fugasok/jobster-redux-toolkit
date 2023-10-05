6aed2db1930bb723317f4d2041e2ceab63f84870
Tibor <tibor736@hotmail.com>
2023-10-05 19:33:37 +0300

    Created dashboard components. SharedLayout structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import FormRow from './FormRow'
 import Logo from './Logo'
+import BigSidebar from './BigSidebar'
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
 
-export { Logo, FormRow }
+export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar }
