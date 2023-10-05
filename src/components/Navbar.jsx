4301e535967b4e0f373f68ad953124a370176dab
Tibor <tibor736@hotmail.com>
2023-10-05 19:41:54 +0300

    added styled components for dashboard components. Added react-icons library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
+import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
+
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+	return <Wrapper>
+    
+    <h2>Navbar</h2>
+    <FaHome/>
+    </Wrapper>
 }
 export default Navbar
