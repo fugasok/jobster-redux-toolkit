69d852fcefb616e6f3ab7bca7e202a50f9d6ab95
Tibor <tibor736@hotmail.com>
2023-10-08 23:49:10 +0300

    jobs container. Loading component

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,6 +1,39 @@
+import { useEffect } from 'react'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
+import { useDispatch, useSelector } from 'react-redux'
+import Loading from './Loading'
+
 const JobsContainer = () => {
-  return (
-    <div>JobsContainer</div>
-  )
+	const { jobs, isLoading } = useSelector((store) => store.allJobs)
+	const dispatch = useDispatch()
+
+	if (isLoading) {
+		return (
+			<Wrapper>
+				<Loading center='center'/>
+			</Wrapper>
+		)
+	}
+
+	if (jobs.lenght === 0) {
+		return (
+			<Wrapper>
+				<h2>No jobs found</h2>
+			</Wrapper>
+		)
+	}
+
+	return (
+		<Wrapper>
+			<h5>Jobs info</h5>
+			<div className='jobs'>
+				{jobs.map((job) => {
+					console.log(job)
+					return <Job key={job._id} {...job} />
+				})}
+			</div>
+		</Wrapper>
+	)
 }
 export default JobsContainer
