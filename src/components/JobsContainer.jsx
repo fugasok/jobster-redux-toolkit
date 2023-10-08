ffbf1e8de0b8531f5fd28a3905cc3cc9693276a7
Tibor <tibor736@hotmail.com>
2023-10-09 00:01:49 +0300

    fetching all jobs list

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -3,15 +3,20 @@ import Job from './Job'
 import Wrapper from '../assets/wrappers/JobsContainer'
 import { useDispatch, useSelector } from 'react-redux'
 import Loading from './Loading'
+import { getAllJobs } from '../features/allJobs/allJobsSlice'
 
 const JobsContainer = () => {
 	const { jobs, isLoading } = useSelector((store) => store.allJobs)
 	const dispatch = useDispatch()
 
+	useEffect(() => {
+		dispatch(getAllJobs())
+	}, [])
+
 	if (isLoading) {
 		return (
 			<Wrapper>
-				<Loading center='center'/>
+				<Loading center />
 			</Wrapper>
 		)
 	}
