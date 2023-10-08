1ecf5457d3b5a0429350855db793c5865a3af934
Tibor <tibor736@hotmail.com>
2023-10-08 23:02:57 +0300

    logout message is shown when there is payload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
 					<button
 						className='dropdown-btn'
 						type='button'
-						onClick={() => dispatch(logoutUser())}
+						onClick={() => dispatch(logoutUser('Logging out...'))}
 					>
 						logout
 					</button>
