da9a0d113406614f5136ef2eb22b5038673fd75c
Tibor <tibor736@hotmail.com>
2023-10-08 22:48:49 +0300

    created createJob functionality. ExtraReducers added for createJob

--- src/pages/Dashboard/AddJob.jsx
+++ src/pages/Dashboard/AddJob.jsx
@@ -2,7 +2,11 @@ import { FormRow, FormRowSelect } from '../../components'
 import Wrapper from '../../assets/wrappers/DashboardFormPage'
 import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
-import { handleChange, clearValues } from '../../features/job/jobSlice'
+import {
+	handleChange,
+	clearValues,
+	createJob,
+} from '../../features/job/jobSlice'
 
 const AddJob = () => {
 	const {
@@ -25,6 +29,7 @@ const AddJob = () => {
 			toast.error('Please fill out all fields')
 			return
 		}
+		dispatch(createJob({ position, company, jobLocation, jobType, status }))
 	}
 
 	const handleJobInput = (e) => {
