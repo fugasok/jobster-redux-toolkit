04faea48a5a2ad17648eb977a4f616e2e8ce83a5
Tibor <tibor736@hotmail.com>
2023-10-08 23:12:20 +0300

    created components for All jobs page. Added to index

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Logo from './Logo'
 import BigSidebar from './BigSidebar'
 import Navbar from './Navbar'
 import SmallSidebar from './SmallSidebar'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+import Job from './Job'
 
-export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar, FormRowSelect }
+export {
+	Logo,
+	FormRow,
+	BigSidebar,
+	Navbar,
+	SmallSidebar,
+	FormRowSelect,
+	SearchContainer,
+	JobsContainer,
+	Job,
+}
