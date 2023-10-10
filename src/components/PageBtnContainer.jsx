60e48eb0f19026f58a121e50a40a4266c9695569
Tibor <tibor736@hotmail.com>
2023-10-11 00:02:40 +0300

    next and prev page buttons

--- src/components/PageBtnContainer.jsx
+++ src/components/PageBtnContainer.jsx
@@ -1,6 +1,7 @@
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 import Wrapper from '../assets/wrappers/PageBtnContainer'
 import { useSelector, useDispatch } from 'react-redux'
+import { changePage } from '../features/allJobs/allJobsSlice'
 
 const PageBtnContainer = () => {
 	const { numOfPages, page } = useSelector((store) => store.allJobs)
@@ -10,8 +11,20 @@ const PageBtnContainer = () => {
 		return index + 1
 	})
 
-	const nextPage = () => {}
-	const prevPage = () => {}
+	const nextPage = () => {
+		let newPage = page + 1
+		if (newPage > numOfPages) {
+			newPage = 1
+		}
+		dispatch(changePage(newPage))
+	}
+	const prevPage = () => {
+		let newPage = page - 1
+		if (newPage < 1) {
+			newPage = numOfPages
+		}
+		dispatch(changePage(newPage))
+	}
 
 	return (
 		<Wrapper>
@@ -28,7 +41,7 @@ const PageBtnContainer = () => {
 							className={
 								pageNumber === page ? 'pageBtn active' : 'pageBtn'
 							}
-							onClick={() => console.log('change page')}
+							onClick={() => dispatch(changePage(pageNumber))}
 						>
 							{pageNumber}
 						</button>
